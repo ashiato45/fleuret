c8f15c3a196d2892f4e9ef9cead2b9e2d6f9caee
ashiato45 <katiiijp@mail.com>
2014-12-27 22:00:32 +0900

    Stick

--- Assets/InputControl.cs
+++ Assets/InputControl.cs
@@ -90,13 +90,42 @@ public class InputControl
         return power;
     }
 
+    public static Vector2 normalizeStick(Vector2 power)
+    {
+        if (Mathf.Abs(power.x) > 0.01 || Mathf.Abs(power.y) > 0.01)
+        {
+            if (Mathf.Abs(power.x) > Mathf.Abs(power.y))
+            {
+                var nowd = power.magnitude;
+                var bigd = nowd / Mathf.Abs(power.x);
+                power.x /= bigd;
+                power.y /= bigd;
+            }
+            else if (Mathf.Abs(power.x) < Mathf.Abs(power.y))
+            {
+                var nowd = power.magnitude;
+                var bigd = nowd / Mathf.Abs(power.y);
+                power.x /= bigd;
+                power.y /= bigd;
+            }
+            else
+            {
+                power.x /= Mathf.Sqrt(2);
+                power.y /= Mathf.Sqrt(2);
+            }
+        }
+        return power;
+    }
+
     public static Vector2 getPowerStick()
     {
         Vector2 power = Vector2.zero;
         power.x = Input.GetAxis("Horizontal");
         power.y = Input.GetAxis("Vertical");
 
-        UnityEngine.Debug.Log(power.x.ToString() + "." + power.y.ToString());
+        power = normalizeStick(power);
+        UnityEngine.Debug.Log("L:" + power.x.ToString() + "," + power.y.ToString() + "," + power.magnitude);
+
 
         //power = power / Mathf.Sqrt(2);
 
@@ -108,7 +137,8 @@ public class InputControl
         Vector2 power = Vector2.zero;
         power.x = Input.GetAxis("Horizontal2");
         power.y = Input.GetAxis("Vertical2");
-        UnityEngine.Debug.Log(power.x.ToString() + "," + power.y.ToString());
+        power = normalizeStick(power);
+        UnityEngine.Debug.Log("R:" + power.x.ToString() + "," + power.y.ToString() + "," + power.magnitude);
 
         //power = power / Mathf.Sqrt(2);
 
